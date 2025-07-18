c16f55bfebff06df02a7db51a99414167ca65f8a
Andy <scene@scene.com>
2025-07-18 15:24:19 +0000

    Renamed to unshackle, same tool different name.

--- automatarr/__main__.py
+++
@@ -1,3 +0,0 @@
-if __name__ == "__main__":
-    from automatarr.core.__main__ import main
-    main()
--- automatarr/core/proxies/__init__.py
+++
@@ -1,5 +0,0 @@
-from .basic import Basic
-from .hola import Hola
-from .nordvpn import NordVPN
-
-__all__ = ("Basic", "Hola", "NordVPN")
